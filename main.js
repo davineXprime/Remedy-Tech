f553437c8b3b5abf0747b38c69cac8e7b4e261b1
Taylovak <110338424+davineXprime@users.noreply.github.com>
2026-09-03 13:38:25 -0400

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-const affiliateLink = "https://amzn.to/4xzHX7w";
-
-document.getElementById("buyButton").addEventListener("click", () => {
-  window.open(affiliateLink, "_blank", "noopener,noreferrer");
-});
