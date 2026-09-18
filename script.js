f9d7366cceae89043c19ed95fd5cdd0fe5f87732
Taylovak <110338424+davineXprime@users.noreply.github.com>
2026-09-18 15:17:53 -0400

    Update and rename script.jss to script.js

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
   btn.addEventListener("click", () => {
 
-    fetch("./article.html")   // IMPORTANT: relative path for GitHub
+    fetch("./article.html")   // GitHub Pages requires this
       .then(res => res.text())
       .then(html => {
         document.getElementById("article-container").innerHTML = html;
       })
       .catch(err => {
         document.getElementById("article-container").innerHTML =
-          "<p style='color:red;'>Could not load article.</p>";
+          "<p style='color:red;'>Could not load article. Check folder structure.</p>";
         console.error(err);
       });
 
